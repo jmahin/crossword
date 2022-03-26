b54aa53e3faa47768487e028e267aaf294d37332
jmahin <mahinjo@icloud.com>
2022-03-26 13:57:31 -0400

    it1

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function hideSide() {
+    var hideSide = document.getElementById(".sidebar");
+    hideSide.parentNode.removeChild(hideSide);
+}
+
+// sessionto keep track of user logged in
+
+// for top player list, render only first two letters of name 
