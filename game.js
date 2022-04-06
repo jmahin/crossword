2ef970f644078ab7b912e28b0cf049ddfd919eb5
jmahin <mahinjo@icloud.com>
2022-04-05 22:56:26 -0400

    .

--- game.js
+++ game.js
@@ -6,6 +6,9 @@ function () {
 }
 // need to pull words from db
 
+function retrieveWords() {
+    // some sort of command to pull words from DB
+}
 // need to obtain dummy data OR download a dictionary
 
 // functionality re: difficulty level + no of words, not to be implemented atm
@@ -24,13 +27,12 @@ function () {
 
 // future functionality: inhibit incorrect input?? or signal to user they're wrong?
 
-
 // add submit button
 
 // print to user the score
 
 // send score to db
 
-
+// for internal use, need to log generation time 
 
 
