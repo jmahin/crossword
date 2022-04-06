a94485557f6cfec3cdb812abea9d7426f5fee2d0
jmahin <mahinjo@icloud.com>
2022-04-06 18:21:16 -0400

    changes

---
+++ game2.js
@@ -0,0 +1,18 @@
+// HANG MAN
+
+// pull word from DB
+
+// print def to user
+
+// provide # of spaces in word or phrase
+
+// user can input letters
+
+// need loop, where incorrect letter is rejected and hang man is created
+
+// GUI????
+
+// when there is correct input, text changes color + user receives congratualatory message
+
+// if logged in, score is pushed to db
+
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,3 +7,29 @@ function hideSide() {
 
 // for top player list, render only first two letters of name 
 
+// HIDE + OPEN SIDEBAR
+
+// CURSOR THAT GETS BIGGER when hovering over div(game)
+
+// keyboard with letters for user to enter
+
+//if not too heavy, then i need sound effects
+ // one sound for adding, + one removing
+
+// PASSWORD, NEED A FUNCTION TO RENDER ASTERISKS
+
+// javascript for forgot password page, enter password, loop (restart if user submits empty string), if user enters an email, send a message indicating a password may have been sent to email associated with the account
+// obfuscate whether an account+email actually is in db 
+// need security for this
+
+// javascript for the forms
+
+const topPlayers = [
+    { name: 'username1', type: '5000'},
+    { name: 'username2', type: '4000'},
+    { name: 'username3', type: '3000'},
+    { name: 'username4', type: '2000' },
+    { name: 'username5', type: '1000'},
+]
+
+// req DOM, create a function, select the table, append child for the row + column
