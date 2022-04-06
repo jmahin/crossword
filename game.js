b35bc6d5240f297a1af999a0adc16307ce453958
jmahin <mahinjo@icloud.com>
2022-04-05 22:52:41 -0400

    .

---
+++ game.js
@@ -0,0 +1,36 @@
+// need a loading message
+
+function () {
+    console.log(`Generating a puzzle.`);
+    document.getElementById("game").innerText = "Loading...";
+}
+// need to pull words from db
+
+// need to obtain dummy data OR download a dictionary
+
+// functionality re: difficulty level + no of words, not to be implemented atm
+
+// pull 10 words from db
+
+// determine intersections 
+
+// if a word can't intersect, then DISCARD word from puzzle
+
+// DOM print puzzle to front end
+
+// add sound effects for adding + removing
+
+// add w javascript a keyboard w letters
+
+// future functionality: inhibit incorrect input?? or signal to user they're wrong?
+
+
+// add submit button
+
+// print to user the score
+
+// send score to db
+
+
+
+
